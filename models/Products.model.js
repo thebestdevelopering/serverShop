a8422d0984fa6a6ff9fa985553c508e2a20635bc
Aliev <thebestdevelopering@gmail.com>
2021-07-29 10:22:52 +0300

    edit-1

---
+++ models/Products.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: String, 
+  
+});
+
+const Products = mongoose.model("Products", bookSchema);
+module.exports = Products;
