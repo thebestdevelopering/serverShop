7280df605d2d30aee5f2b6dc981ab00673391b20
Aliev <thebestdevelopering@gmail.com>
2021-07-29 13:03:24 +0300

    edit-3

--- models/Products.model.js
+++ models/Products.model.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
   name: String,
+  price: Number,
+  brand: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brend",
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
 });
 
 const Products = mongoose.model("Products", productSchema);
